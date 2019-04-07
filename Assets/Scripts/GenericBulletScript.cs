d61354a578dc9e24428e35f5aaeb679fa8ed88de
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2019-04-07 16:50:38 +0300

    New weapon system

---
+++ Assets/Scripts/GenericBulletScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericBulletScript : MonoBehaviour
+{
+    public float damage = 1.0f;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Asteroid")
+        {
+            col.gameObject.SendMessage("Damage", damage);
+            Destroy(this.gameObject);
+        }
+    }
+}

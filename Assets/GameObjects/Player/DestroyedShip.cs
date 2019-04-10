30d24f9049d8739a871cc798df55a3ba6e720280
Barrett Kepler <Kepler-br@yandex.ru>
2019-04-10 10:57:21 +0300

    Add destroyedship

---
+++ Assets/GameObjects/Player/DestroyedShip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyedShip : MonoBehaviour
+{
+    Transform[] childrens;
+    public Quaternion rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = Quaternion.Euler(0.0f, 0.0f, 0.9f);
+        childrens = this.GetComponentsInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        foreach(Transform children in childrens)
+        {
+            children.rotation *= rot;
+        }
+    }
+}
